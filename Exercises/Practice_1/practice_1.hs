41c497a7f62896a4394b570263421f9d61cac905
Mariano D'Angelo <madang@taltech.ee>
2022-09-23 14:17:53 +0300

    beautified

--- Exercises/Practice_1/practice_1.hs
+++ Exercises/Practice_1/practice_1.hs
@@ -1,40 +1,36 @@
 -- 1. find if an integer is smaller, greater, or equal to 100
 isNumLessGreaterEqualToHun :: Int -> [Char]
-isNumLessGreaterEqualToHun number =
-    if number > 100
-        then show number ++ " is greater than 100."
-    else if number < 100
-        then show number ++ " is smaller than 100."
-    else show number ++ " is equal to 100."
+isNumLessGreaterEqualToHun number
+  | number > 100 = show number ++ " is greater than 100."
+  | number < 100 = show number ++ " is smaller than 100."
+  | otherwise = show number ++ " is equal to 100."
 
 
 -- 2. calculate the circumference and area of a circle with given radius
-calculateCircumference :: Double -> Double 
-calculateCircumference radius = 2 * pi * radius 
-calculateArea :: Double -> Double 
+calculateCircumference :: Double -> Double
+calculateCircumference radius = 2 * pi * radius
+
+calculateArea :: Double -> Double
 calculateArea radius = radius ** 2 * pi
 
 
 -- 3. calculate euclidian distance between two points (x1, y1) and (x2, y2)
-findEuclidianDistance :: Double -> Double -> Double -> Double -> Double 
+findEuclidianDistance :: Double -> Double -> Double -> Double -> Double
 findEuclidianDistance x2 x1 y2 y1 = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 
 -- 4. find if year is leap or not
 findLeapYear :: Integer -> [Char]
-findLeapYear year =
-    if mod year 400 == 0
-        then show year ++ " is a leap year."
-    else if mod year 100 == 0
-        then show year ++ " is not a leap year."
-    else if mod year 4 == 0
-        then show year ++ " is a leap year."
-    else show year ++ " is not a leap year."
+findLeapYear year
+  | mod year 400 == 0 = show year ++ " is a leap year."
+  | mod year 100 == 0 = show year ++ " is not a leap year."
+  | mod year 4 == 0 = show year ++ " is a leap year."
+  | otherwise = show year ++ " is not a leap year."
 
 
 -- EVERYTHING MUST BE AS FUNCTION BE CALLED IN MAIN
 main :: IO()
-main = do 
+main = do
 
     -- task number 1
     putStr "\n"
