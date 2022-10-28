23fba58f5e4a8724f296b0986bb47953f1aa0c53
Mariano D'Angelo <madang@taltech.ee>
2022-10-28 15:49:20 +0300

    calll

--- Assignments/Assignment_1/main.hs
+++ Assignments/Assignment_1/main.hs
@@ -1,7 +1,11 @@
-toLength :: [String] -> Integer -> [String]
-toLength strings x = []
+toLength :: Integer -> [String] -> [String]
+toLength x strings = []
 
 
 main :: IO()
 main = do
     putStrLn "Hello, Assignment 1!"
+    -- task 1
+    let strings = ["a", "b", "cd", "def"]
+    print (toLength 5 strings)
+    print (toLength 2 strings)
