59998cca5440cd6f2dd2d31e178c1d74bc8f0831
Mariano D'Angelo <madang@taltech.ee>
2022-10-28 17:48:17 +0300

    task 1 done

--- Assignments/Assignment_1/main.hs
+++ Assignments/Assignment_1/main.hs
@@ -1,11 +1,13 @@
-mapStrings :: [String] -> [String]
-mapStrings [] = []
-mapStrings (x:xs) = map (x ++) (x:xs)
-
-toLength :: [String] -> [String]
-toLength [] = []
-toLength (x:xs) = take (length (x:xs)^2) (mapStrings (x:xs) ++ toLength ys)
+-- TASK 1
+combineStrings :: [String] -> [String]
+combineStrings [] = []
+combineStrings (x:xs) = take (length (x:xs)^2) (catStrings ++ combineStrings ys)
     where ys = xs ++ [x]
+          catStrings = map (x ++) (x:xs)
+
+
+toLength :: Int -> [String] -> [String]
+toLength n xs = [str | str <- combineStrings xs, length str == n]
 
 
 main :: IO()
@@ -13,6 +15,5 @@ main = do
     putStrLn "Hello, Assignment 1!"
     -- task 1
     let strings = ["a", "b", "cd", "def"]
-    let stringo = ["a", "e", "i", "o", "u"]
-    print (toLength strings)
-    print (toLength stringo)
+    print (toLength 5 strings)
+    print (toLength 2 strings)
