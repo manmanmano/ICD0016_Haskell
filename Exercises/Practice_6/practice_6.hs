03f2f2b1c589a5c06ccdbd6da4c4e142ef6601d6
Mariano D'Angelo <madang@taltech.ee>
2022-11-20 20:31:53 +0200

    finish

--- Exercises/Practice_6/practice_6.hs
+++ Exercises/Practice_6/practice_6.hs
@@ -2,12 +2,17 @@ import Data.Foldable (toList)
 
 
 -- Task 1
-
+calculateRoots :: (Double, Double, Double) -> (Maybe Double, Maybe Double)
+calculateRoots (a, b, c) = (Just x1, Just x2) 
+    where
+        x1 = (-b + sqrt y) / (2 * a)
+        x2 = (-b - sqrt y) / (2 * a)
+        y = (b * b) - (4 * a * c)
 
 
 -- Task 2
-incrementAll :: Functor f => Num a => f a -> f a
-incrementAll = fmap (+1)
+incrementAll :: (Functor f, Num a) => f a -> f a
+incrementAll item = (+1) <$> item
 
 
 -- Task 3
@@ -16,16 +21,23 @@ count item = length . filter (== item) . toList
 
 
 main ::IO()
-main = do 
+main = do
+
     putStrLn "Hello practice 6!\n"
 
+    -- Task 1
+    putStrLn "Function that calculates quadratic formula roots (NaN when not existing): "
+    print (calculateRoots (-4, 7, 2))
+    print (calculateRoots (11, 9, 2))
+
     -- Task 2
-    putStrLn "Function incrementAll results: "
+    putStrLn "\nFunction incrementAll results: "
     print (incrementAll [1,2,3])
     print (incrementAll (Just 3.0))
-    putStrLn ""
 
     -- Task 3
-    putStrLn "Function count results: "
+    putStrLn "\nFunction count results: "
     print(count True [True,False,True])
     print(count 'c' (Just 'c'))
+
+    putStrLn "\nDone!"
