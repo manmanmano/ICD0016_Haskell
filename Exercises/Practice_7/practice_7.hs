7374a7ab335f1cdfc93e8cf2e8612348df513162
Mariano D'Angelo <madang@taltech.ee>
2022-12-05 00:00:51 +0200

    done with practices

--- Exercises/Practice_7/practice_7.hs
+++ Exercises/Practice_7/practice_7.hs
@@ -1,4 +1,34 @@
 import Data.Maybe (isNothing)
+import Data.Char (isDigit, isUpper)
+
+
+-- Task 1
+split :: String -> Maybe (String, String)
+split s =
+    case names of
+        [forename, surname] -> Just (forename, surname)
+        _ -> Nothing
+    where names = words s
+
+
+checkNumber :: (String, String) -> Maybe (String, String)
+checkNumber (forename, surname)
+    | containsNumber forename = Nothing
+    | containsNumber surname = Nothing
+    | otherwise = Just (forename, surname)
+    where containsNumber = any isDigit
+
+
+checkCapital :: (String, String) -> Maybe (String, String)
+checkCapital (forename, surname)
+    | isUpper (head forename) && isUpper (head surname) = Just (forename, surname)
+    | otherwise = Nothing
+
+
+readNames s =
+    split s >>=
+        checkNumber >>=
+            checkCapital
 
 
 -- Task 2
@@ -19,14 +49,26 @@ establishWinner scores firstPlayer secondPlayer = do
 main :: IO()
 main = do
 
-    putStrLn "Hello practice 7!\n"
+    putStrLn "Hello practice 7!"
+
+    -- Task 1
+    putStrLn "\nNames that could be read: "
+    print (readNames "Lebron James")
+    print (readNames "Lionel Messi")
+    print (readNames "Lewis Hamilton")
+    print (readNames "FrankSinatra") -- no space
+    print (readNames "Wall 3ee") -- contains number
+    print (readNames "Gigi proietti") -- no capital letter
+
 
     -- Task 2
     let scores = [("Kaur", 34), ("Joonas", 63), ("Maria", 34), ("Tarmo", 27)]
 
-    putStrLn "Established winners: "
+    putStrLn "\nEstablished winners: "
     print (establishWinner scores "Kaur" "Joonas")
     print (establishWinner scores "Maria" "Tarmo")
     print (establishWinner scores "Kaur" "Maria")
     print (establishWinner scores "Johannes" "Joonas")
     print (establishWinner scores "Kaur" "Rasmus")
+
+    putStrLn "\nDone!"
