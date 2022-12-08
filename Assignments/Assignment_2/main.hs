0abb1250b8b7d0ef9adcf33c56684e4fe4e0a3a3
Mariano D'Angelo <madang@taltech.ee>
2022-12-08 17:17:54 +0200

    start

--- Assignments/Assignment_2/main.hs
+++ Assignments/Assignment_2/main.hs
@@ -1,3 +1,30 @@
+import Text.Read (readMaybe)
+import Data.Maybe (isJust)
+
+
+-- Task 1
+-- readNames s =
+--     split s >>=
+--         checkNumber >>=
+--             checkCapital
+
+units :: Int -> Maybe String
+units n 
+    | n == 0 = Just "zero"
+    | n > 0 && n < 10 = Just (unitsWords !! n)
+    | otherwise = Nothing
+    where 
+        unitsWords = words "one two three four five six seven eight nine"
+
+
+tens :: Int -> Maybe String
+tens n 
+    | n > 10 && n < 20 = Just (unitsWords !! n)
+    | otherwise = Nothing
+    where 
+        unitsWords = words "eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen"
+
+
 -- Task 2
 data TwoList a = TwoEmpty | TwoNode a a (TwoList a) deriving Show
 
@@ -12,10 +39,19 @@ instance Foldable TwoList where
     foldr func initialValue (TwoNode first second list) = func first (func second (foldr func initialValue list))
 
 
-
 main :: IO()
 main = do
-    putStrLn "Hello, assignment 2!"
+
+    -- Task 1
+    putStrLn "\nInput a natural number in the range 0-999999: "
+    str <- getLine
+    let num = readMaybe str :: Maybe Integer
+    if isJust num && num <= Just 999999
+        then putStrLn "ok"
+    else do 
+        putStrLn "Input must be a natural number less than 1000000!"
+        main
+
 
     -- Task 2, Part 1
     putStrLn "\nFunctor TwoList: "
@@ -33,5 +69,8 @@ main = do
     print(product (TwoNode 1 2 (TwoNode 3 4 TwoEmpty)))
     putStr "length: "
     print(length (TwoNode 1 2 (TwoNode 3 4 TwoEmpty)))
+
+    -- Task 3
+
 
     putStrLn "\nDone!"
